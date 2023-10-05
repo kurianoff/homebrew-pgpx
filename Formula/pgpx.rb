54263036103732c3aa8ec3775903c4ccc65af3d5
Constantine Kurianoff <kurianoff@gmail.com>
2023-10-05 01:19:15 -0400

    fixed linter errors

--- Formula/pgpx.rb
+++ Formula/pgpx.rb
@@ -1,15 +1,15 @@
 class Pgpx < Formula
-  desc "Single-backend PostgreSQL proxy for enabling RDS IAM-based authentication with dynamic passwords on Postgres client apps that do not support dynamic credentials."
+  desc "Single-backend PostgreSQL proxy that enables RDS IAM authentication on Postgres client apps"
   homepage "https://github.com/kurianoff/pgpx"
-  version "0.0.4"
+  version "0.0.5"
   license "Mozilla Public License Version 2.0"
 
   if Hardware::CPU.arm?
     url "https://github.com/kurianoff/pgpx/releases/download/v#{version}/pgpx-darwin-arm64"
-    sha256 "9f36851715bcda1e959004a10a3d7d71b8923b6267dfacde94ba2b1356ea97f8"
+    sha256 "574aa95e51231d7e284d99184fdf5cc033d4714981f4f1b6a16b25e97905e184"
   else
     url "https://github.com/kurianoff/pgpx/releases/download/v#{version}/pgpx-darwin-amd64"
-    sha256 "3bc7f1ce0693f938d239713dca6bde542b8e91576b09ad37d37653cebe4df40f"
+    sha256 "2d85acac9a0ebcab30ca84062bbe53a774b2f55ee39091233d7e5864dfb14799"
   end
 
   def install
